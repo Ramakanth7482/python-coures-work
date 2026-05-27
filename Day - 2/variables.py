1dad42f825809091cc1316e7551bc71fa508b0ce
G.Ramakanth Reddy <ramakanthgelajigari2414@gmail.com>
2026-05-27 17:55:46 +0530

    Add files via upload

---
+++ Day - 2/variables.py
@@ -0,0 +1,62 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============================================== RESTART: C:/Users/Ramakanth Reddy/OneDrive/Desktop/python-course-work/Day - 2/key words.py ==============================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+
+============================================== RESTART: C:/Users/Ramakanth Reddy/OneDrive/Desktop/python-course-work/Day - 2/key words.py ==============================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+myvar=10
+MYvar1=10
+myvar@1=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+my_var=10
+my var=10
+SyntaxError: invalid syntax
+>>> myvar=10
+>>> MYVar=10
+>>> myvara+b=20+10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> myvar= a+b
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    myvar= a+b
+NameError: name 'a' is not defined
+>>> Myvar=a+b=10+20
+SyntaxError: cannot assign to expression
+>>> var=10
+>>> var=20
+>>> var
+20
+>>> var
+20
+>>> a,b,c=10,20,30
+>>> a
+10
+>>> a
+10
+>>> a
+10
+>>> 
+>>> bb
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    bb
+NameError: name 'bb' is not defined. Did you mean: 'b'?
+>>> b
+20
+>>> b
+20
+>>> c
+30
+>>> a=b=c=10,20,30
+>>> a
+(10, 20, 30)
+>>> b
+(10, 20, 30)
+>>> 
+============================================== RESTART: C:/Users/Ramakanth Reddy/OneDrive/Desktop/python-course-work/Day - 2/key words.py ==============================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
