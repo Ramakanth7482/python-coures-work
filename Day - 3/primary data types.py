0e97f766c0744dc73ec5ae1ac2eeeba239a9d4a8
G.Ramakanth Reddy <ramakanthgelajigari2414@gmail.com>
2026-05-28 22:13:38 +0530

    Add files via upload

---
+++ Day - 3/primary data types.py
@@ -0,0 +1,34 @@
+print("hello")
+1.numeric
+
+int :- whole numbers {0-9}
+float :- decimal numbers {10.0}
+complex :- real - imaginary numbers {2+3j}
+
+
+a = 10
+type(a)
+<class 'int'>
+
+2.sequencal
+
+string :- str = name (immutable)
+list :- list = ["apple", "banana"](mutable)
+tuple :- tuple = numbers (immutable)
+
+        
+3.mapping
+
+set :- frozenset
+dictionary (dict)
+
+4. boolen
+
+True
+False
+
+
+5. none
+
+
+
